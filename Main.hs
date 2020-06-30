70915a28c31a04ea7984256c386cc32e35bed900
jhuizy <jhuizenga99@gmail.com>
2020-06-30 14:10:51 +1000

    basic OCR working

--- Main.hs
+++ Main.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Data.Text as T
+import Turtle
+import Prelude hiding (FilePath)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = readPdf "./assets/receipt.png" >>= print
+
+readPdf :: FilePath -> IO [Text]
+readPdf file = do
+  dump <- strict $ inproc tesseract args empty
+  return $ T.lines dump
+  where
+    tesseract = "tesseract"
+    args = [format fp file, "stdout", "-l", "eng"]
