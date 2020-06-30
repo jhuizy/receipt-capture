8ce6a49cbe3a457a89cdecc014004efb969cb2c5
jhuizy <jhuizenga99@gmail.com>
2020-06-30 13:15:37 +1000

    initial commit

---
+++ shell.nix
@@ -0,0 +1,27 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  # inherit pkgs;
+  gitignoreSource = (import sources."gitignore.nix" { inherit (pkgs) lib;}).gitignoreSource;
+  project = pkgs.haskellPackages.callCabal2nix "receipt-capture" (gitignoreSource ./.) { };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; project.env.nativeBuildInputs ++ [
+    haskellPackages.cabal-install
+    haskellPackages.ghcide
+    haskellPackages.cabal2nix
+    haskellPackages.hlint
+    haskellPackages.ormolu
+    haskellPackages.cabal2nix
+    haskellPackages.ghcid
+    coreutils
+    gitAndTools.pre-commit
+    tesseract4
+  ];
+  shellHook = ''
+    export NIX_GHC="`which ghc`"
+    export NIX_GHCPKG="`which ghc`/../ghc-pkg"
+    export NIX_GHC_DOCDIR="`which ghc`/../../share/doc/ghc/html"
+    export NIX_GHC_LIBDIR=$( $NIX_GHC --print-libdir )
+  '';
+}
