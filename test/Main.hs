d6b30ff88fef803cfae0dde9ae0434fc405326b1
jhuizy <jhuizenga99@gmail.com>
2020-09-18 14:20:32 +1000

    add test that I had forgot to commit

--- test/Main.hs
+++ test/Main.hs
@@ -1,3 +1,14 @@
 import Test.Hspec
-import App
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import App hiding (main)
+import Model
 
+main :: IO ()
+main = check prop_ListReverses
+
+prop_ListReverses :: Property
+prop_ListReverses = property $ do
+  xs <- forAll $ Gen.list (Range.linear 0 100) Gen.alpha
+  reverse (reverse xs) === xs
